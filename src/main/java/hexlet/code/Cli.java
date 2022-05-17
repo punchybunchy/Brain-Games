e3a3bef27c74e8c0b95cecde4b4563c83013023a
tyoma <artyomlosev.work@gmail.com>
2022-05-17 18:20:42 +0300

    Add Cli.java

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void inputName(){
+        System.out.print("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
+        scanner.close();
+    }
+}
