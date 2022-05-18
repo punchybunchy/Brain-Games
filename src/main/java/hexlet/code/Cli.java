836d12fefb899398e2407eef071591fdaeaa1f6d
tyoma <artyomlosev.work@gmail.com>
2022-05-18 15:02:19 +0300

    Fix bug

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -2,7 +2,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void inputName(){
+    public static void inputName() {
         System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
