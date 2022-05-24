bbd140a9d431a717d7ffd2ec09dfbd95ddf86d8d
tyoma <artyomlosev.work@gmail.com>
2022-05-24 13:50:59 +0300

    Update Calc.java

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -4,34 +4,38 @@ import hexlet.code.Engine;
 
 public class Calc {
 
-    static final int a = 1;
-    static final int b = 100;
+    static final int A = 1;
+    static final int B = 10;
+    static final int C = 100;
+    static final int ARR_SIZE = 100;
 
-    static String[] questions  = new String[100];
-    static String[] answers = new String[100];
-    static String description = "What is the result of the expression?";
+    private static String[] questions  = new String[ARR_SIZE];
+    private static String[] answers = new String[ARR_SIZE];
+    private static String description = "What is the result of the expression?";
 
     public static void runCalc() {
-        for (int index = 0; index < 100; index++){
+        for (int index = 0; index < ARR_SIZE; index++) {
 
-            int firstNumber = a + (int) (Math.random() * b);
+            int firstNumber = A + (int) (Math.random() * B);
             String firstStrNumber = Integer.toString(firstNumber);
 
-            int secondNumber = a + (int) (Math.random() * b);
+            int secondNumber = A + (int) (Math.random() * B);
             String secondStrNumber = Integer.toString(secondNumber);
 
             int mathResult = 0;
             String expression = " ";
+            final int limitValue1 = 33; // this var needs to chose operator (+/-/*) in a random manner
+            final int limitValue2 = 66;
 
-            var x = a + (int) (Math.random() * b);
+            var x = A + (int) (Math.random() * C);
 
-            if (x <= 33) {
+            if (x <= limitValue1) {
                 mathResult = firstNumber + secondNumber;
                 expression = firstStrNumber + " + " + secondStrNumber;
-            } else if (x > 33 && x <= 66) {
+            } else if (x > limitValue1 && x <= limitValue2) {
                 mathResult = firstNumber - secondNumber;
                 expression = firstStrNumber + " - " + secondStrNumber;
-            } else if (x > 66) {
+            } else if (x > limitValue2) {
                 mathResult = firstNumber * secondNumber;
                 expression = firstStrNumber + " * " + secondStrNumber;
             }
