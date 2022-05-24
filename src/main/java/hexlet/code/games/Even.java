600cc2825f85583c80a23f639cceb4081a537099
tyoma <artyomlosev.work@gmail.com>
2022-05-24 13:51:29 +0300

    Update Even.java

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -3,17 +3,17 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Even {
-    static final int a = 1; //constant to define the minimum of a random number, can be modified
-    static final int b = 10; //constant to define the maximum of power of number, can be modified
+    static final int A = 1; //constant to define the minimum of a random number, can be modified
+    static final int B = 10; //constant to define the maximum of power of number, can be modified
+    static final int ARR_SIZE = 100;
+    private static String[] questions = new String[ARR_SIZE]; // 100 questions max value of a questions pull
+    private static String[] answers = new String[ARR_SIZE];
 
-    static String[] questions = new String[100]; // 100 questions max value of a questions pull
-    static String[] answers = new String[100];
-
-    static String description = "Answer 'yes' if number even otherwise answer 'no'.";
+    private static String description = "Answer 'yes' if number even otherwise answer 'no'.";
 
     public static void runEven() {
-        for (int i = 0; i < 100; i++) {
-            int randomNumber = a + (int) (Math.random() * b);
+        for (int i = 0; i < ARR_SIZE; i++) {
+            int randomNumber = A + (int) (Math.random() * B);
             questions[i] = Integer.toString(randomNumber);
             if (randomNumber % 2 == 0) {
                 answers[i] = "yes";
